2e14b0857e0acf843d4b50c18b4c6991507b1a06
saketha <krishnasaketha800@gmail.com>
2026-02-07 21:30:03 +0530

    feat: add Footer component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 export function Layout() {
     return (
@@ -11,6 +12,7 @@ export function Layout() {
                 <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
                     <Outlet />
                 </main>
+                <Footer />
             </div>
         </div>
     )
